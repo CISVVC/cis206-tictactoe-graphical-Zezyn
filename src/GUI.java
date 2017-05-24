e1f57d511349291f5de2007a8e9bd7b00ee4c496
Abel <7607920511m.h@gmail.com>
2017-05-24 12:42:40 -0700

    Project creation

---
+++ src/GUI.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Wizard on 5/24/2017.
+ */
+
+import javax.swing.JFrame;
+
+public class GUI {
+    public static void Frame() {
+        JFrame frame = new JFrame();
+
+        final int FRAME_WIDTH = 300;
+        final int FRAME_HEIGHT = 400;
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setTitle("Tic Tac Toe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void createBoard() {
+
+    }
+
+    public static void createX() {
+
+    }
+
+    public static void createO() {
+
+    }
+}
