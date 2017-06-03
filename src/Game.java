d49af6c8da5e712283da81021ff9b9b39239429b
Abel <7607920511m.h@gmail.com>
2017-06-03 11:36:50 -0700

    Code Cleanup. Starting creation of New Game button and Winner Window

--- src/Game.java
+++ src/Game.java
@@ -4,14 +4,16 @@
  */
 
 public class Game {
-    static String playerMark;
-    static int player;
-    static Board board;
-    static String[] winnerCheck = new String[10];
-    static int count = 1;
+
+    //Global Variables
+    static String playerMark; //Used to keep track of players x or o
+    static int player; //Used to keep track of player 1 or 2
+    static String[] winnerCheck = new String[10]; //Used to keep track of button number
+    static int count = 1; //Used to keep count in winnerCheck
 
 
     public static String changePlayer() {
+        //Checks to see if player 1 is active and changes the players mark for player 2's turn
         if (player == 1 || player == 0) { playerMark = "O"; player = 2; }
         else { playerMark = "X"; player = 1; }
         return playerMark;
@@ -19,143 +21,41 @@ public class Game {
 
     public static void checkWinner(Object d) {
 
+        //Parses the object into an integer
         count = Integer.parseInt(d.toString());
 
+        //Adds int to winnerCheck array by parses object to a string
         winnerCheck[count] = d.toString();
 
-        for(String myValue : winnerCheck) {
-            System.out.println(winnerCheck[count] + " " + count + " " + myValue);
-        }
+        //Used to check variables
+        /*for(String buttonNumber : winnerCheck) {
+            System.out.println(winnerCheck[count] + " " + count + " " + buttonNumber);
+        }*/
+
+            //Check for winner in top row;
+            if(winnerCheck[1] == "1" && winnerCheck[2] == "2" && winnerCheck[3] == "3")
+                { System.out.println("Winner top row"); }
+            //Check winner in middle row
+            else if(winnerCheck[4] == "4" && winnerCheck[5] == "5" && winnerCheck[6] == "6")
+                { System.out.println("Winner mid row"); }
+            //Check winner bottom row
+            else if(winnerCheck[7] == "7" && winnerCheck[8] == "8" && winnerCheck[9] == "9")
+                { System.out.println("Winner bottom row"); }
+            //Check winner left column
+            else if(winnerCheck[1] == "1" && winnerCheck[4] == "4" && winnerCheck[7] == "7")
+                { System.out.println("Winner left col"); }
+            //Check winner middle column
+            else if(winnerCheck[2] == "2" && winnerCheck[5] == "5" && winnerCheck[8] == "8")
+                { System.out.println("Winner mid col"); }
+            //Check winner right column
+            else if(winnerCheck[3] == "3" && winnerCheck[6] == "6" && winnerCheck[9] == "9")
+                { System.out.println("Winner right col"); }
+            //Check winner right to left diagonal
+            else if(winnerCheck[1] == "1" && winnerCheck[5] == "5" && winnerCheck[9] == "9")
+                { System.out.println("Winner left to right diag"); }
+            //Check winner left to right diagonal
+            else if(winnerCheck[7] == "7" && winnerCheck[5] == "5" && winnerCheck[3] == "3")
+                { System.out.println("Winner right to left diag"); }
+
     }
 }
-    /*
-    public static void doTurn() {
-
-        char player = 'X';
-        int row = 0, col = 0;
-        boolean won = true;
-        boolean tie = true;
-
-        do {
-
-            won = isWon(board);
-            tie = isBoardFull(board);
-
-            if (isWon(board)) {
-                System.out.print("\nPlayer " + player + " dominated! \n ++GAME OVER!++\n");
-                break;
-            } else if (isBoardFull(board)) {
-                System.out.println("\n++TIE GAME!++\n");
-                break;
-            }
-
-            //Puts in player char in the proper locations
-            int spot;
-            System.out.print(" -- Player " + player + " \n ");
-            spot = 0;
-            switch (spot) {
-                case 1:
-                    row = 0;
-                    col = 0;
-                    break;
-                case 2:
-                    row = 0;
-                    col = 1;
-                    break;
-                case 3:
-                    row = 0;
-                    col = 2;
-                    break;
-                case 4:
-                    row = 1;
-                    col = 0;
-                    break;
-                case 5:
-                    row = 1;
-                    col = 1;
-                    break;
-                case 6:
-                    row = 1;
-                    col = 2;
-                    break;
-                case 7:
-                    row = 2;
-                    col = 0;
-                    break;
-                case 8:
-                    row = 2;
-                    col = 1;
-                    break;
-                case 9:
-                    row = 2;
-                    col = 2;
-                    break;
-                default:
-                    System.out.print("Invalid Entry: Try again\n");
-                    break;
-            }
-
-            if (row >= 0 && row < board.length && col >= 0 && col < board[0].length) {
-
-                if (board[row][col] == 'X' || board[row][col] == 'O') {
-                    System.out.println("Invalid\n");
-                    doTurn(board);
-                } else {
-                    board[row][col] = player;
-                    player = changePlayer(player);
-                }
-            }
-        } while(!won || !tie);
-    }
-
-    public static boolean isBoardFull(char[][] board) {
-        boolean isFull = true;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] != 'X' && board[i][j] != 'O') {
-                    isFull = false;
-                }
-            }
-        }
-        return isFull;
-    }
-
-    public static boolean isWon(char[][] board) {
-
-        if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') { //Checks horizontal
-            return true;
-        } else if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') {
-            return true;
-        } else if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') {
-            return true;
-        } else if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') {
-            return true;
-        } else if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') {
-            return true;
-        } else if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') {
-            return true;
-        } else if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') { //Checks vertical
-            return true;
-        } else if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') {
-            return true;
-        } else if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') {
-            return true;
-        } else if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') {
-            return true;
-        } else if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') {
-            return true;
-        } else if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') {
-            return true;
-        } else if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') { //Checks diagonal
-            return true;
-        } else if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
-            return true;
-        } else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
-            return true;
-        } else if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
-            return true;
-        } else { return false; }
-    }
-}
-*/
